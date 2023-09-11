cfdd0cd29993dd4a88e94957904255d3ba3369af
Admin <kseniatsytrikova@gmail.com>
2023-09-11 21:24:44 +0300

    restyle pagination into custom design

--- src/components/Main/Pagination/Table-pagination.tsx
+++ src/components/Main/Pagination/Table-pagination.tsx
@@ -1,23 +1,30 @@
 import React, { useState } from 'react'
-import Stack from '@mui/material/Stack'
 import { useAppDispatch } from '../../../common/hooks/use-app-dispatch'
 import { getCurrenciesThunk } from '../main-slice'
-import { Pagination } from '@mui/material'
 import style from './pagination.module.scss'
+import ReactPaginate from 'react-paginate';
 
 export const TablePagination = () => {
   const [page, setPage] = useState(1)
 
   let dispatch = useAppDispatch()
-  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    setPage(value)
-      dispatch(getCurrenciesThunk(value-1))
+  const handleChange = (selectedItem: { selected: number; }) => {
+    setPage(selectedItem.selected)
+      dispatch(getCurrenciesThunk(selectedItem.selected))
 
   }
 
   return (
-    <Stack spacing={2} className={style.container}>
-      <Pagination count={Math.ceil(2000/15)} page={page} onChange={handleChange} />
-    </Stack>
+    <ReactPaginate
+      className={style.container}
+      breakLabel="..."
+      nextLabel=">"
+      onPageChange={handleChange}
+      pageRangeDisplayed={15}
+      pageCount={2000/15}
+      previousLabel="<"
+      renderOnZeroPageCount={null}
+    />
+
   )
 }
